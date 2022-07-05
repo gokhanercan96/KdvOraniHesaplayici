cf157a9c775fe91c0b8215486b10f9f986aaf68e
gokhanercan96 <107113396+gokhanercan96@users.noreply.github.com>
2022-07-05 17:07:16 +0300

    Update KdvCalculator.java

--- ProjectKDV/src/KdvCalculator.java
+++ ProjectKDV/src/KdvCalculator.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class KdvCalculator {
     public static void main(String[] args) {
-        double urunFiyati, kdvOrani, kdvliFiyat;
-        boolean kdvOraniBelirleme;
+        
+        double urunFiyati, kdvOrani, kdvliFiyat;     
+        
         Scanner inp = new Scanner(System.in);
 
         System.out.print("Ürünün Fiyatı (tl): ");
         urunFiyati = inp.nextDouble();
 
-        kdvOraniBelirleme = (urunFiyati > 0) && (urunFiyati < 1000);
-        kdvOrani = kdvOraniBelirleme ? 0.18d : 0.08d;
-        System.out.println("KDV Oranı: " + kdvOrani);
+        kdvOrani = (urunFiyati<1000)? 0.18 : 0.08;
+        System.out.println("KDV Oranı: " +kdvOrani);
 
-        kdvliFiyat = (urunFiyati * kdvOrani) + urunFiyati;
-        System.out.print("Ürünün KDV'li Fiyatı: " + kdvliFiyat);
+        kdvliFiyat = ((urunFiyati*kdvOrani)+urunFiyati);
+        System.out.println("KDV'li Ürün Fiyatı: " +kdvliFiyat);
 
     }
 }
